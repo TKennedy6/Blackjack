d0eef3ae74262b9ae0339b98f1b5757d77ca41a0
Tim Kennedy <tim.kennedy6@yahoo.com>
2025-07-28 08:16:01 -0700

    Initial commit

---
+++ blackjack.js
@@ -0,0 +1,6 @@
+const deckOfCards = {
+    spades: ['king', 'queen', 'jack', 'ace', 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    clubs: ['king', 'queen', 'jack', 'ace', 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    hearts: ['king', 'queen', 'jack', 'ace', 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    diamonds: ['king', 'queen', 'jack', 'ace', 2, 3, 4, 5, 6, 7, 8, 9, 10]
+}
